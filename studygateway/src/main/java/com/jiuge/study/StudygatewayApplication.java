af651d64f2302cf6b05fc43bdfb58672776748d9
huwei <shuiwuying@foxmail.com>
2021-08-13 09:40:12 +0800

    添加网关应用

---
+++ studygateway/src/main/java/com/jiuge/study/StudygatewayApplication.java
@@ -0,0 +1,13 @@
+package com.jiuge.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudygatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudygatewayApplication.class, args);
+    }
+
+}
--- studyuser/src/test/java/com/jiuge/user/StudyuserApplicationTests.java
+++ studyuser/src/test/java/com/jiuge/user/StudyuserApplicationTests.java
@@ -3,11 +3,18 @@ package com.jiuge.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 @SpringBootTest
 class StudyuserApplicationTests {
 
     @Test
     void contextLoads() {
+        ZonedDateTime zonedDateTime = ZonedDateTime.now();//默认时区
+        // 用指定时区获取当前时间
+        ZonedDateTime zonedDateTime2 = ZonedDateTime.now(ZoneId.of("Asia/Shanghai"));
+        System.out.println(zonedDateTime2);
     }
 
 }
